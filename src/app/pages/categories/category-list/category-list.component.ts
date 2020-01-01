9ea5794bf3618a76fc8900af362c5a7ca1763ace
Anderson <andersonribeiro.si@hotmail.com>
2020-01-01 15:20:32 -0300

    Adicionado ação de deletar algum elemento da lista de categorias

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
+import { CategoryModel } from '../shared/category.model';
+import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  private categories: CategoryModel[] = [];
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Lista retornada com sucesso')
+    )
+  }
+
+  delete(category) {
+    const mustDelete = confirm("Deseja realmente deletar este item?")
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category)
+      )
+    }
   }
 
 }
