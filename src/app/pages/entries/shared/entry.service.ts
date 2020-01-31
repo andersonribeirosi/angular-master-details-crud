a4f8faf060a89926ec2c00085b298ca55f585e1d
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2020-01-30 23:27:11 -0300

    Improving reports

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Injector } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { flatMap, catchError } from 'rxjs/operators';
+import { flatMap, catchError, map } from 'rxjs/operators';
 
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { CategoryService } from '../../categories/shared/category.service';
 import { EntryModel } from './entry.model';
+
+import * as moment from 'moment'
 
 
 @Injectable({
@@ -28,6 +30,12 @@ export class EntryService extends BaseResourceService<EntryModel> {
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
+  getByMonthAndYear(month: number, year: number):Observable<EntryModel[]>{
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
   protected setCategoryAndSendToServer(entry: EntryModel, sendFunction: any): Observable<EntryModel> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
@@ -37,4 +45,15 @@ export class EntryService extends BaseResourceService<EntryModel> {
       catchError(this.handleError)
     );
   }
+
+  private filterByMonthAndYear(entries: EntryModel[], month: number, year: number){
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, "DD/MM/YYYY");
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) return entry
+    })
+  }
+
 }
--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { CategoryModel } from '../../categories/shared/category.model';
+import { EntryModel } from '../../entries/shared/entry.model';
+import { CategoryService } from '../../categories/shared/category.service';
+import { EntryService } from '../../entries/shared/entry.service';
+
+import currencyformatter from "currency-formatter"
+
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +14,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  expenseTotal: any = 0;
+  revenueTotal: any = 0;
+  balance: any = 0;
+
+  expenseChartData: any;
+  revenueChartData: any;
+
+  ChartOptions = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
+  categories: CategoryModel[] = [];
+  entries: EntryModel[] = [];
+
+  @ViewChild('month') month: ElementRef = null;
+  @ViewChild('year') year: ElementRef = null;
+
+  constructor(private categoryService: CategoryService,
+              private entryService: EntryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll()
+    .subscribe(categories => this.categories = categories);
   }
+
+  generateReports(){
+    const month = this.month.nativeElement.value;
+    const year = this.year.nativeElement.value;
+
+    if(!month || !year){
+      alert('Você precisa escolher um mês e um ano para gerar o relatório')
+    } else {
+      this.entryService.getByMonthAndYear(month, year).subscribe(this.setValues.bind(this))
+    }
+  }
+
+  private setValues(entries: EntryModel[]){
+    this.entries = entries;
+    this.calculateBalance();
+  }
+
+  private calculateBalance(){
+    let expenseTotal = 0;
+    let revenueTotal = 0;
+
+    this.entries.forEach(entry => {
+      if(entry.type == 'revenue')
+        revenueTotal += currencyformatter.unformat(entry.amount, {code: 'BRL'})
+      else {
+        expenseTotal += currencyformatter.unformat(entry.amount, {code: 'BRL'})
+      }
+      });
+      this.expenseTotal = currencyformatter.format(expenseTotal, {code: 'BRL'})
+      this.revenueTotal = currencyformatter.format(revenueTotal, {code: 'BRL'})
+      this.balance = currencyformatter.format(revenueTotal - expenseTotal, {code: 'BRL'});
+    }
 
 }
